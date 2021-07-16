309676d4aa7459291a3a24c59120dc4edd4af58d
venkateswarlu <venkateswarluk123@gmail.com>
2021-07-16 17:29:25 +0530

    Base

---
+++ SampleWebAPI/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SampleWebAPI.Models;
+using System.Threading.Tasks;
+
+namespace SampleWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpPost("Login")]
+       public async Task<IActionResult> Login()
+        {
+            System.IO.StreamReader reader = new System.IO.StreamReader(Request.Body);
+            string data = await reader.ReadToEndAsync();
+            if (!string.IsNullOrEmpty(data))
+            {
+                User user = JsonConvert.DeserializeObject<User>(data);
+                if (user.Email == "venkateswarluk123@gmail.com" && user.Password == "test@123")
+                {
+                    user.Status = "User Logged In successfully";
+                    return Ok(user);
+                }
+                else
+                {
+                    user.Status = "Invalid User.";
+                    return StatusCode(500, user);
+                }
+            }
+            
+            return StatusCode(500, data);
+        }
+    }
+}
---
+++ SampleWebAPI/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleWebAPI.Models
+{
+    public class User
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Status { get; set; }
+    }
+}
